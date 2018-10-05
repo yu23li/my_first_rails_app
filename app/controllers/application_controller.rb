5637ed429d7464dc103b22bf0270a984b3dff9d1
Yuli <yuli23amelia@gmail.com>
2018-10-05 14:00:04 +0700

    add new action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "hola, mundo!"
+  end
+
+  def goodbey
+    render html: "goodbye, world!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#hello'
+
+  root 'application#goodbye'
 end
